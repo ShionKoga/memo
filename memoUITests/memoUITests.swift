e846a91aa8735d9ef7c9c5c5206905ef42e3a518
ShionKoga <shion_koga@mail.toyota.co.jp>
2021-12-03 11:49:39 +0900

    add UI test

--- memoUITests/memoUITests.swift
+++ memoUITests/memoUITests.swift
@@ -1,39 +1,61 @@
-//
-//  memoUITests.swift
-//  memoUITests
-//
-//  Created by 古賀史苑 on 2021/11/26.
-//
-
 import XCTest
 
 class memoUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+
     }
 
     func testExample() throws {
-        // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
+        
+        //メモ追加画面に遷移
+        app.navigationBars["All memo"].buttons["Compose"].tap()
+        let titleTextField = app.textFields["title_text_field"]
+        let detailTextView = app.textViews["detail_text_view"]
+        
+        //タイトルの入力
+        titleTextField.tap()
+        titleTextField.typeText("a")
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //内容の入力
+        detailTextView.tap()
+        detailTextView.typeText("abcde")
+        
+        //保存して元の画面に戻る
+        app.navigationBars["new memo"].buttons["Save"].tap()
+        
+        //一番下のセルをタップ
+        let cells = app.cells.matching(identifier: "title_cell")
+        let cellsCount = cells.count
+        let lastCell = cells.element(boundBy: cellsCount - 1)
+        lastCell.tap()
+        print(app.textViews["detail_text"].value ?? "nil")
+        
+        //内容を編集
+        app.textViews["detail_text"].tap()
+        app.textViews["detail_text"].typeText("zzzz")
+        
+        //元の画面に戻る
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+        
+        //変更が効いてるか確認
+        lastCell.tap()
+        print(app.textViews["detail_text"].value ?? "nil")
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+        
+        //追加したメモを削除
+        lastCell.swipeLeft()
+        lastCell.buttons["Delete"].tap()
     }
-
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
